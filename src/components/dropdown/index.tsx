46111b27d57571de465d604b664bfdfbc2d61bb2
SvirskyiAndrii <a.svirskyi@ghostdrive.com>
2022-12-12 17:42:48 +0200

    Fixes

--- src/components/dropdown/index.tsx
+++ src/components/dropdown/index.tsx
@@ -4,22 +4,22 @@ const data = [
   {
     id: 'new',
     title: 'New possibilities with ghostdrive',
-    data: '1We created the first ai folder using ai and web3 technology. Any data you upload to that folder will automatically regenerate new data with more options. image-to-image and text-to-text modes You will be surprised at how much time you will save in the future.',
+    data: 'We created the first ai folder using ai and web3 technology. Any data you upload to that folder will automatically regenerate new data with more options. image-to-image and text-to-text modes You will be surprised at how much time you will save in the future.',
   },
   {
     id: 'next',
     title: 'next gen of WORKSPACES',
-    data: '2We created the first ai folder using ai and web3 technology. Any data you upload to that folder will automatically regenerate new data with more options. image-to-image and text-to-text modes You will be surprised at how much time you will save in the future.',
+    data: 'We created the first ai folder using ai and web3 technology. Any data you upload to that folder will automatically regenerate new data with more options. image-to-image and text-to-text modes You will be surprised at how much time you will save in the future.',
   },
   {
     id: 'build',
     title: 'Build an AI-Driven DATA',
-    data: '3We created the first ai folder using ai and web3 technology. Any data you upload to that folder will automatically regenerate new data with more options. image-to-image and text-to-text modes You will be surprised at how much time you will save in the future.',
+    data: 'We created the first ai folder using ai and web3 technology. Any data you upload to that folder will automatically regenerate new data with more options. image-to-image and text-to-text modes You will be surprised at how much time you will save in the future.',
   },
   {
     id: 'data',
     title: 'DATA COMMUNICATION and security',
-    data: '4We created the first ai folder using ai and web3 technology. Any data you upload to that folder will automatically regenerate new data with more options. image-to-image and text-to-text modes You will be surprised at how much time you will save in the future.',
+    data: 'We created the first ai folder using ai and web3 technology. Any data you upload to that folder will automatically regenerate new data with more options. image-to-image and text-to-text modes You will be surprised at how much time you will save in the future.',
   },
 ];
 
